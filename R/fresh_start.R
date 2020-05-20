17929d4a5657fb7379e734f7c36a1474a61e376f
Fodil Ihaddaden <ihaddaden.fodeil@gmail.com>
2020-05-20 05:44:10 +0100

    fresh_start done !

--- R/fresh_start.R
+++ R/fresh_start.R
@@ -22,6 +22,14 @@ fresh_start <- function(){
 
   if (decision == "YES"){
 
+    decision2 <- switch(utils::menu(choices = c("NO", "No Way!", "No !!!", "Yes", "Let me think a little bit"),
+                                   title="Are you really really really sure about it ?"),"NO", "NO", "NO", "YES", "NO")
+
+
+  }
+
+  if(decision2 == "YES"){
+
 
 
     insta <- utils::installed.packages()
@@ -32,9 +40,9 @@ fresh_start <- function(){
 
     paths <-   unlist(purrr::map(packages, ~fs::path_package(.)))
 
+    names <- fs::path_split(paths) %>% sapply(., utils::tail, 1)
 
-    fs::dir_delete(paths)
-
+    remove.packages(names)
 
   } else {
 
