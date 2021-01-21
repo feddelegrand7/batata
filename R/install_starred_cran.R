65f0f34600b396399afcddaa8418f269e8c7c0dc
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-01-21 20:26:30 +0100

    adding some tests

--- R/install_starred_cran.R
+++ R/install_starred_cran.R
@@ -13,35 +13,40 @@
 
 
 install_starred_cran <- function(github_user, n = 5) {
-
   if (!is.character(github_user)) {
-
-    stop('github user must be provided as a character string')
-
+    stop("'github_user' must be provided as a character string")
   }
 
-  if(!is.numeric(n)) {
-
-    stop("the 'n' parameter must be numeric")
-
+  if (!is.numeric(n) || n < 1) {
+    stop("the 'n' parameter must be numeric and greater than 1")
   }
 
   data <- jsonlite::fromJSON(glue::glue("https://api.github.com/users/{github_user}/starred?per_page={n}"))
 
   cran_packages <- data[data$language == "R", ]$name
 
-  cran_packages_no_na <- Filter(function(x) {!is.na(x)}, cran_packages)
+  cran_packages_no_na <- Filter(function(x) {
+    !is.na(x)
+  }, cran_packages)
 
-  combine <- function(..., sep = ", ") {
-    paste(..., collapse = sep)
-  }
+  if(length(cran_packages_no_na) == 0) {
 
-  message(glue::glue("the following repositories, if R packages and availables on CRAN, will be installed:
+    message("I can't find any R starred package, go starr some !")
+
+  } else {
+
+    combine <- function(..., sep = ", ") {
+      paste(..., collapse = sep)
+    }
+
+    message(glue::glue("the following repositories, if R packages and availables on CRAN, will be installed:
                      {combine(cran_packages_no_na)}"))
+    purrr::map(
+      cran_packages_no_na,
+      purrr::safely(~ utils::install.packages(.x))
+    )
 
-
-  purrr::map(cran_packages_no_na,
-             purrr::safely(~utils::install.packages(.x)))
+  }
 
 }
 
@@ -56,48 +61,53 @@ install_starred_cran <- function(github_user, n = 5) {
 #' see <install_github()> from the 'remote' package.
 #'
 #' @return called for the side effect of installing the Github starred repositories
-#'
 #' @export
 #' @description installs the Github starred repositories from Github and not from CRAN.
 
 
 install_starred_github <- function(github_user, n = 5, upgrade = "never") {
-
   if (!is.character(github_user)) {
-
     stop("'github_user' must be provided as a character string")
-
   }
 
-  if(!is.numeric(n)) {
-
-    stop("the 'n' parameter must be numeric")
-
+  if (!is.numeric(n) || n < 1) {
+    stop("the 'n' parameter must be numeric and greater than 1")
   }
 
-  if (!(upgrade %in% c('never', 'always'))) {
-
+  if (!(upgrade %in% c("never", "always"))) {
     stop(glue::glue("upgrade takes only the following arguments:
                     - 'never'
                     - 'always' "))
-
   }
 
   data <- jsonlite::fromJSON(glue::glue("https://api.github.com/users/{github_user}/starred?per_page={n}"))
 
   github_r_repos <- data[data$language == "R", ]$full_name
 
-  github_r_repos_no_na <- Filter(function(x) {!is.na(x)}, github_r_repos)
+  github_r_repos_no_na <- Filter(function(x) {
+    !is.na(x)
+  }, github_r_repos)
 
-  combine <- function(..., sep = ", ") {
-    paste(..., collapse = sep)
+  if (length(github_r_repos_no_na) == 0) {
+
+    message("I can't find any R starred package, go starr some !")
+
+  } else {
+
+    combine <- function(..., sep = ", ") {
+      paste(..., collapse = sep)
+    }
+
+    message(glue::glue("the following repositories, if R packages, will be installed:
+                     {combine(github_r_repos_no_na)}"))
+    purrr::map(
+      github_r_repos_no_na,
+      purrr::safely(~ remotes::install_github(.x, upgrade = upgrade))
+    )
+
+
   }
 
-  message(glue::glue("the following repositories, if R packages, will be installed:
-                     {combine(github_r_repos_no_na)}"))
-
-  purrr::map(github_r_repos_no_na,
-             purrr::safely(~remotes::install_github(.x, upgrade = upgrade)))
 
 }
 
@@ -113,11 +123,8 @@ install_starred_github <- function(github_user, n = 5, upgrade = "never") {
 
 
 install_most_starred <- function(n = 10) {
-
-  if (!is.numeric(n)) {
-
-    stop("the 'n' parameter must be numeric not ", typeof(n))
-
+  if (!is.numeric(n) || n < 1) {
+    stop("the 'n' parameter must be numeric and greater than 1")
   }
 
 
@@ -127,7 +134,9 @@ install_most_starred <- function(n = 10) {
 
   most_starred <- data$items.name
 
-  most_starred_no_na <- Filter(function(x) {!is.na(x)}, most_starred)
+  most_starred_no_na <- Filter(function(x) {
+    !is.na(x)
+  }, most_starred)
 
 
   combine <- function(..., sep = ", ") {
@@ -137,8 +146,8 @@ install_most_starred <- function(n = 10) {
   message(glue::glue("the following repositories, if availables on CRAN, will be installed:
                      {combine(most_starred_no_na)}"))
 
-  purrr::map(most_starred_no_na,
-             purrr::safely(~utils::install.packages(.x)))
-
-
+  purrr::map(
+    most_starred_no_na,
+    purrr::safely(~ utils::install.packages(.x))
+  )
 }
---
+++ tests/testthat/test-install_starred_cran.R
@@ -0,0 +1,47 @@
+
+data <- jsonlite::fromJSON(glue::glue("https://api.github.com/users/feddelegrand7/starred?per_page=5"))
+
+test_that("expect an error when wrong parameters provided", {
+
+  expect_error(
+
+    install_starred_cran(github_user = 123123,
+                         n = 10),
+    regexp = "'github_user' must be provided as a character string"
+
+      )
+
+  expect_error(
+
+    install_starred_cran(github_user = "feddelegrand7",
+                         n = "10"),
+    regexp = "the 'n' parameter must be numeric and greater than 1"
+  )
+
+
+  expect_error(
+
+    install_starred_cran(github_user = "feddelegrand7",
+                         n = 0),
+    regexp = "the 'n' parameter must be numeric and greater than 1"
+  )
+
+})
+
+
+test_that("test if the function is returning a data frame", {
+
+
+  expect_s3_class(data, class = "data.frame")
+
+
+})
+
+
+
+test_that("test if the function returns a number of rows corresponding to 'n'", {
+
+  expect_equal(nrow(data), 5)
+
+
+})
