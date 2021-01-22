9ece6cf5b7596cd8761844c62b90f5b0d90d088d
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2021-01-22 23:14:51 +0100

    adding curl to the imports so that the tests  fail

--- R/install_starred_cran.R
+++ R/install_starred_cran.R
@@ -2,7 +2,7 @@
 
 #' Install Github Starred CRAN Packages
 #'
-#' @param github_user the Github repo user name
+#' @param github_user the Github user name to look for
 #' @param n the last 'n' starred repositories. Defaults to 5, in which case it will look for
 #' the last 5 starred repositories, filter the R repos and install them
 #' @return called for the side effect of installing the Github starred packages
@@ -53,7 +53,7 @@ install_starred_cran <- function(github_user, n = 5) {
 
 #' Install Github Starred Packages from Github
 #'
-#' @param github_user the Github repo user name
+#' @param github_user the Github user name to look for
 #' @param n the last 'n' starred repositories. Defaults to 5, in which case it will look for
 #' the last 5 starred repositories, filter the R repos and install them
 #' @param upgrade whether to upgrade out of date packages. You can choose from
@@ -112,6 +112,7 @@ install_starred_github <- function(github_user, n = 5, upgrade = "never") {
 
 }
 
+
 #' Install the most starred CRAN packages
 #'
 #' @param n the most starred starred CRAN packages. Defaults to 10.
@@ -151,4 +152,56 @@ install_most_starred <- function(n = 10) {
     most_starred_no_na,
     purrr::safely(~ utils::install.packages(.x))
   )
+}
+
+
+#' Display User's Github Starred Repositories
+#'
+#' @param github_user the Github user name to look for
+#' @param n the number of the last starred repositories. Defaults to 5 in which case
+#' it will return the last 5 starred repositories. Note that if the 'onlyR' parameter
+#' is set to TRUE, you might get a lower number of starred repos due to filtering R from
+#' all the other languages.
+#' @param onlyR Logical, whether to fetch only R repositories, Default to FALSE
+#'
+#' @return A character vector of starred Github repositories
+#' @export
+#'
+#' @examples
+#'
+#' display_starred("hadley", n = 10, onlyR = FALSE)
+#'
+
+display_starred <- function(github_user, n = 5, onlyR = FALSE) {
+  if (!is.character(github_user)) {
+    stop("'github_user' must be provided as a character string")
+  }
+
+  if (!is.numeric(n) || n < 1) {
+    stop("the 'n' parameter must be numeric and greater than 1")
+  }
+
+  data <- jsonlite::fromJSON(glue::glue("https://api.github.com/users/{github_user}/starred?per_page={n}"))
+
+
+  starred_repo <- if (onlyR) {
+
+    data[data$language == "R", ]$name
+
+  } else {
+
+    data$name
+
+  }
+
+  if(length(starred_repo) == 0) {
+
+    message("I can't find any R starred package, go starr some !")
+
+  } else {
+
+   return(starred_repo)
+
+  }
+
 }
